5c0d22fad886b10ae0596b06f8e7f2ab12964d61
Christo <christo@devatstation.games>
2024-03-12 23:10:49 -0400

    Changed tabs for spaces

--- lua/gothic/config/init.lua
+++ lua/gothic/config/init.lua
@@ -10,9 +10,10 @@ vim.opt.smartcase = true
 vim.opt.hlsearch = true
 vim.opt.showmode = true
 vim.opt.scrolloff = 20
-vim.opt.tabstop = 4
+-- vim.opt.tabstop = 4
 -- vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
 
 vim.opt.cursorline = true
 vim.opt.updatetime = 250
