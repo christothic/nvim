ff20d9e697bc5aedc47fac2eff8159f36db8032f
Christo <christopher.amato@gmail.com>
2024-03-16 03:34:49 -0400

    Added ai code generator (Codeum)

--- lua/gothic/plugins/autoformater.lua
+++ lua/gothic/plugins/autoformater.lua
@@ -3,13 +3,17 @@ local M = {}
 M.plugin = { -- Autoformat
     "stevearc/conform.nvim",
     opts = {
-        notify_on_error = false,
+        notify_on_error = true,
         format_on_save = {
             timeout_ms = 500,
             lsp_fallback = true,
         },
         formatters_by_ft = {
             lua = { "stylua" },
+            -- h = { "asp-grep" },
+            -- cpp = { "asp-grep" },
+            -- h = { "clang-format" },
+            -- cpp = { "clang-format" },
             -- Conform can also run multiple formatters sequentially
             -- python = { "isort", "black" },
             --
