9d0eae0ffc66a518f5da555f4299cbf309704208
Christo <christopher.amato@gmail.com>
2025-05-24 21:15:22 -0400

    fixed init

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
-require("gothic")
+if not vim.g.vscode then
+    if vim.fn.has("macunix") then
+        vim.cmd("redir! > ~/.config/nvim/last_log.txt")
+    else
+        vim.cmd("redir! > ~\\AppData\\Local\\nvim\\last_log.txt")
+    end
+end
+
+vim.g.config_username = "gothic" -- use this to change config folders
+require(vim.g.config_username)
