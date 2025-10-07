f7fae3d6e1320bec96d68286cffaa0b3d020cb0c
Christo <christopher.amato@gmail.com>
2025-10-07 13:39:58 -0400

    Small changes

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,5 +1,28 @@
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*.md",
+vim.api.nvim_create_autocmd({
+    "BufEnter",
+    "BufWinEnter"
+}, {
+    pattern = { "vifmrc" },
+    callback = function()
+        -- vim.treesitter.start()
+        vim.opt.commentstring = "\" %s"
+    end
+})
+vim.api.nvim_create_autocmd({
+    "BufEnter",
+    "BufWinEnter"
+}, {
+    pattern = { "*.yuck" },
+    callback = function()
+        vim.treesitter.start()
+        vim.opt.commentstring = ";; %s"
+    end
+})
+vim.api.nvim_create_autocmd({
+    "BufEnter",
+    "BufWinEnter"
+}, {
+    pattern = { "*.md" },
     callback = function()
         vim.treesitter.start()
     end
